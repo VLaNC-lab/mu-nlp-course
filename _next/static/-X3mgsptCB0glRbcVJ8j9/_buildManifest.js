55851ce41de02ff0b5879bf6201638f81ce61184
vlanc lab <vlanc-lab@gmail.com>
2026-02-13 15:34:23 +0530

    Deploy static site

---
+++ _next/static/-X3mgsptCB0glRbcVJ8j9/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/mu-nlp-course//_next/:path+",
+        "destination": "/mu-nlp-course/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
